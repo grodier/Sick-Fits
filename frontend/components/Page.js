92ca0a75e05d92b80fe27b1b39f0f870e8271d22
George.Rodier <grodier3@gmail.com>
2018-12-13 12:56:08 -0500

    Initial page and header layout. Add nextjs custom app

---
+++ frontend/components/Page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Meta from '../components/Meta';
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Page;
+
