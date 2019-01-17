3b9d90bc15db365c5b61f2cd5961f545ada87a6f
George.Rodier <grodier3@gmail.com>
2019-01-17 17:16:55 -0500

    Add query for single item; Add update of single item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -12,6 +12,19 @@ const Mutations = {
     );
 
     return item;
+  },
+  updateItem(parent, args, context, info) {
+    //first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return context.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
   }
 };
 
